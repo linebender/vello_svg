4e16b55bedb403f0511b26de3f2d58ac36ff5385
Spencer C. Imbleau <spencer@imbleau.com>
2024-04-19 19:36:53 -0400

    fix: copyright header

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,6 @@
+// Copyright 2023 the Vello Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 use std::convert::Infallible;
 use vello::kurbo::{Affine, BezPath, Point, Rect, Stroke};
 use vello::peniko::{Blob, Brush, Color, Fill, Image};
