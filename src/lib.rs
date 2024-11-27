8ad0228eddcedab0d151e140e2e44cd5291ae723
Kaur Kuut <strom@nevermore.ee>
2024-11-27 12:00:00 +0200

    Mention `nextest`.

--- src/lib.rs
+++ src/lib.rs
@@ -103,7 +103,7 @@ pub fn append_tree_with<F: FnMut(&mut vello::Scene, &usvg::Node)>(
 
 #[cfg(test)]
 mod tests {
-    // CI will fail unless it can execute at least one test per workspace.
+    // CI will fail unless cargo nextest can execute at least one test per workspace.
     // Delete this dummy test once we have an actual real test.
     #[test]
     fn dummy_test_until_we_have_a_real_test() {}
