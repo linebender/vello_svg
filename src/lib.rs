e7bccb7c86529b720098720eba7d0dd81d20b9f7
Kaur Kuut <strom@nevermore.ee>
2024-11-27 11:52:41 +0200

    Add a dummy test to satisfy `nextest`.

--- src/lib.rs
+++ src/lib.rs
@@ -100,3 +100,11 @@ pub fn append_tree_with<F: FnMut(&mut vello::Scene, &usvg::Node)>(
 ) {
     render::render_group(scene, svg.root(), Affine::IDENTITY, error_handler);
 }
+
+#[cfg(test)]
+mod tests {
+    // CI will fail unless it can execute at least one test per workspace.
+    // Delete this dummy test once we have an actual real test.
+    #[test]
+    fn dummy_test_until_we_have_a_real_test() {}
+}
