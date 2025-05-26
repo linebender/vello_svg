4b65480b91b418e0a2fb29a9d53ce15baa544801
Nixon <43715558+nixon-voxell@users.noreply.github.com>
2025-05-26 21:18:55 +0800

    Support group opacity & blend modes (#61)
    This is a quick hack to support group opacity, e.g.
    ```svg
    <path opacity="0.5" d="..." fill="#1C274C"/>
    ```
    
    Wanted to note however that some features which is supported in vello is
    not implemented yet, like `BlendMode`
    ```rs
    pub struct Group {
        pub(crate) id: String,
        pub(crate) transform: Transform,
        pub(crate) abs_transform: Transform,
        pub(crate) opacity: Opacity,
        pub(crate) blend_mode: BlendMode,
        pub(crate) isolate: bool,
        pub(crate) clip_path: Option<Arc<ClipPath>>,
        /// Whether the group is a context element (i.e. a use node)
        pub(crate) is_context_element: bool,
        pub(crate) mask: Option<Arc<Mask>>,
        pub(crate) filters: Vec<Arc<filter::Filter>>,
        pub(crate) bounding_box: Rect,
        pub(crate) abs_bounding_box: Rect,
        pub(crate) stroke_bounding_box: Rect,
        pub(crate) abs_stroke_bounding_box: Rect,
        pub(crate) layer_bounding_box: NonZeroRect,
        pub(crate) abs_layer_bounding_box: NonZeroRect,
        pub(crate) children: Vec<Node>,
    }
    ```
    
    Edit: I have gone ahead and fully added a layer per group implementation
    which takes into account of blend modes and opacity.

--- src/render.rs
+++ src/render.rs
@@ -16,27 +16,77 @@ pub(crate) fn render_group<F: FnMut(&mut Scene, &usvg::Node)>(
         let transform = transform * util::to_affine(&node.abs_transform());
         match node {
             usvg::Node::Group(g) => {
-                let mut pushed_clip = false;
-                if let Some(clip_path) = g.clip_path() {
-                    if let Some(usvg::Node::Path(clip_path)) = clip_path.root().children().first() {
-                        // support clip-path with a single path
+                let alpha = g.opacity().get();
+                let mix = match g.blend_mode() {
+                    usvg::BlendMode::Normal => {
+                        if alpha < 1.0 {
+                            vello::peniko::Mix::Normal
+                        } else {
+                            vello::peniko::Mix::Clip
+                        }
+                    }
+                    usvg::BlendMode::Multiply => vello::peniko::Mix::Multiply,
+                    usvg::BlendMode::Screen => vello::peniko::Mix::Screen,
+                    usvg::BlendMode::Overlay => vello::peniko::Mix::Overlay,
+                    usvg::BlendMode::Darken => vello::peniko::Mix::Darken,
+                    usvg::BlendMode::Lighten => vello::peniko::Mix::Lighten,
+                    usvg::BlendMode::ColorDodge => vello::peniko::Mix::ColorDodge,
+                    usvg::BlendMode::ColorBurn => vello::peniko::Mix::ColorBurn,
+                    usvg::BlendMode::HardLight => vello::peniko::Mix::HardLight,
+                    usvg::BlendMode::SoftLight => vello::peniko::Mix::SoftLight,
+                    usvg::BlendMode::Difference => vello::peniko::Mix::Difference,
+                    usvg::BlendMode::Exclusion => vello::peniko::Mix::Exclusion,
+                    usvg::BlendMode::Hue => vello::peniko::Mix::Hue,
+                    usvg::BlendMode::Saturation => vello::peniko::Mix::Saturation,
+                    usvg::BlendMode::Color => vello::peniko::Mix::Color,
+                    usvg::BlendMode::Luminosity => vello::peniko::Mix::Luminosity,
+                };
+
+                let clipped = match g
+                    .clip_path()
+                    // support clip-path with a single path
+                    .and_then(|path| path.root().children().first())
+                {
+                    Some(usvg::Node::Path(clip_path)) => {
                         let local_path = util::to_bez_path(clip_path);
                         scene.push_layer(
                             BlendMode {
-                                mix: vello::peniko::Mix::Clip,
+                                mix,
                                 compose: vello::peniko::Compose::SrcOver,
                             },
-                            1.0,
+                            alpha,
                             transform,
                             &local_path,
                         );
-                        pushed_clip = true;
+
+                        true
                     }
-                }
+                    // Ignore if we're in `Mix::Clip` mode and there's nothing to clip.
+                    _ if !matches!(mix, vello::peniko::Mix::Clip) => {
+                        // Use bounding box as the clip path.
+                        let bounding_box = g.layer_bounding_box();
+                        let rect = vello::kurbo::Rect::from_origin_size(
+                            (bounding_box.x(), bounding_box.y()),
+                            (bounding_box.width() as f64, bounding_box.height() as f64),
+                        );
+                        scene.push_layer(
+                            BlendMode {
+                                mix,
+                                compose: vello::peniko::Compose::SrcOver,
+                            },
+                            alpha,
+                            transform,
+                            &rect,
+                        );
+
+                        true
+                    }
+                    _ => false,
+                };
 
                 render_group(scene, g, Affine::IDENTITY, error_handler);
 
-                if pushed_clip {
+                if clipped {
                     scene.pop_layer();
                 }
             }
@@ -48,41 +98,39 @@ pub(crate) fn render_group<F: FnMut(&mut Scene, &usvg::Node)>(
 
                 let do_fill = |scene: &mut Scene, error_handler: &mut F| {
                     if let Some(fill) = &path.fill() {
-                        match util::to_brush(fill.paint(), fill.opacity()) {
-                            Some((brush, brush_transform)) => {
-                                scene.fill(
-                                    match fill.rule() {
-                                        usvg::FillRule::NonZero => Fill::NonZero,
-                                        usvg::FillRule::EvenOdd => Fill::EvenOdd,
-                                    },
-                                    transform,
-                                    &brush,
-                                    Some(brush_transform),
-                                    &local_path,
-                                );
-                            }
-                            _ => {
-                                error_handler(scene, node);
-                            }
+                        if let Some((brush, brush_transform)) =
+                            util::to_brush(fill.paint(), fill.opacity())
+                        {
+                            scene.fill(
+                                match fill.rule() {
+                                    usvg::FillRule::NonZero => Fill::NonZero,
+                                    usvg::FillRule::EvenOdd => Fill::EvenOdd,
+                                },
+                                transform,
+                                &brush,
+                                Some(brush_transform),
+                                &local_path,
+                            );
+                        } else {
+                            error_handler(scene, node);
                         }
                     }
                 };
                 let do_stroke = |scene: &mut Scene, error_handler: &mut F| {
                     if let Some(stroke) = &path.stroke() {
-                        match util::to_brush(stroke.paint(), stroke.opacity()) {
-                            Some((brush, brush_transform)) => {
-                                let conv_stroke = util::to_stroke(stroke);
-                                scene.stroke(
-                                    &conv_stroke,
-                                    transform,
-                                    &brush,
-                                    Some(brush_transform),
-                                    &local_path,
-                                );
-                            }
-                            _ => {
-                                error_handler(scene, node);
-                            }
+                        if let Some((brush, brush_transform)) =
+                            util::to_brush(stroke.paint(), stroke.opacity())
+                        {
+                            let conv_stroke = util::to_stroke(stroke);
+                            scene.stroke(
+                                &conv_stroke,
+                                transform,
+                                &brush,
+                                Some(brush_transform),
+                                &local_path,
+                            );
+                        } else {
+                            error_handler(scene, node);
                         }
                     }
                 };
