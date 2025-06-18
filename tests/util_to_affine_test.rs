baf6ce1c9e3e38097ba1b158cb36cd8c935d10ce
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-06-18 15:33:04 +0200

    adds regression test for util::to_affine

---
+++ tests/util_to_affine_test.rs
@@ -0,0 +1,23 @@
+#![allow(missing_docs, reason = "regression test for util::to_affine")]
+#[cfg(test)]
+pub mod util_to_affine_test {
+    use vello::kurbo::Affine;
+    use vello_svg::util::to_affine;
+
+    #[test]
+    fn regression_test() {
+        let usvg_transform = usvg::Transform {
+            sx: 1.,
+            kx: 2.,
+            ky: 3.,
+            sy: 4.,
+            tx: 5.,
+            ty: 6.,
+        };
+
+        let result = to_affine(&usvg_transform);
+        let expected = Affine::new([1.0, 3.0, 2.0, 4.0, 5.0, 6.0]);
+
+        assert_eq!(result, expected);
+    }
+}
